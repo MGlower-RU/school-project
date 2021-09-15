eb189d113d0d4084e79e14f9238fb85dcce0ec69
Danil <danilkazaharovich@gmail.com>
2021-09-15 10:14:17 +0300

    If signed in redirect to / from /signin

--- src/components/Routing.js
+++ src/components/Routing.js
@@ -1,14 +1,18 @@
-import { Switch, Route } from "react-router-dom";
+import { useContext } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { LoginContext } from "../App";
 
 import Coupon from './Coupon'
 import Login from "./Login";
 import Payment from "./Payment";
 
 export default function Routing() {
+  const { isLogged } = useContext(LoginContext)
+
   return (
     <Switch>
       <Route path="/signin">
-        <Login />
+        {isLogged === 'true' ? <Redirect to="/" /> : <Login />}
       </Route>
       <Route path="/payment">
         <Payment />
